18cf726d8ad746cd1f8241e6b3bb2f3dd09f7ca2
Pablo Medina <jmedinamulet@gmail.com>
2023-08-16 09:16:22 -0400

    add facebook route

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const craigslist = require('./routes/craigslist');
+const ebay = require('./routes/ebay');
+const facebook = require('./routes/facebook');
+
+
 require('dotenv').config()
 
 app.use(cors())
 
-app.listen(8080, () => {
-    console.log('listening on, ', 8080 );
+app.listen(2121, () => {
+    console.log('listening on, ', 2121 );
   });
 
   app.use('/craigslist', craigslist);
+  app.use('/ebay', ebay);
+  app.use('/facebook', facebook);
+
