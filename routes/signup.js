b1fb8f44b987a5862e2e24ddc39b38d3013c9f07
Pablo Medina <jmedinamulet@gmail.com>
2023-08-24 09:23:35 -0400

    add signup validation

--- routes/signup.js
+++ routes/signup.js
@@ -6,11 +6,26 @@ require("dotenv").config();
 
 router.post('/', (req, res) => {
     const {username, email, password} = req.body;
+    if(!password || !username && !email){
+        return res.status(400).send('Please enter a username or email and password')
+    }
+    if(!email.includes('@') || !email.includes('.')){
+        return res.status(400).send('Email must be valid')
+    }
+    if(password.length < 8){
+        return res.status(400).send('Password must be at least 8 characters')
+    }
+    if(password.includes(username) || password.includes(email.slice(0, email.indexOf('@')))){
+        return res.status(400).send('Password must not contain username')
+    }
+    if(password.includes('password')){
+        return res.status(400).send('Are you for real?')
+    }
     knex('users')
         .where({username})
         .then((users) => {
             if (users.length) {
-                res.status(409).send('User already exists');
+                res.status(409).send('Username already exists');
             } else {
                 knex('users')
                     .insert({username, email, password})
