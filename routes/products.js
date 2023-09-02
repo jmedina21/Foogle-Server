6ca3b2fbb951cb47845171332040ee3473ebcd5f
Pablo Medina <jmedinamulet@gmail.com>
2023-09-02 19:28:32 -0400

    remove unnecesary code

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const knex = require("knex")(require("../knexfile"));
 const authorize = require('../middleware/authorize')
 require("dotenv").config();
 const products = require('../controllers/products-controller')
