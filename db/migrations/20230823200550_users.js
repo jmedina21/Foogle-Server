e7c7d96dd439982a346c5be3aea4b3b008eff7ec
Pablo Medina <jmedinamulet@gmail.com>
2023-09-02 21:16:54 -0400

    update to only include email and password

--- db/migrations/20230823200550_users.js
+++ db/migrations/20230823200550_users.js
@@ -5,7 +5,6 @@
 exports.up = function (knex) {
     return knex.schema.createTable('users', (table) => {
       table.increments('id').primary();
-      table.string('username').notNullable().unique();
       table.string('email').notNullable().unique();
       table.string('password').notNullable();
       table.timestamp('created_at').defaultTo(knex.fn.now());
--- db/seeds/01_users.js
+++ db/seeds/01_users.js
@@ -7,19 +7,16 @@ exports.seed = async function (knex) {
   await knex('users').insert([
     {
       id: 1,
-      username: 'PMcCullough',
       email: 'patmc@gmail.com',
       password: 'pat123',
     },
     {
       id: 2,
-      username: 'JYoung',
       email: 'jyoung@yahoo.com',
       password: 'foreveryoung',
     },
     {
       id: 3,
-      username: 'JPMM21',
       email: 'pmedina@gmail.com',
       password: 'jpmm21',
     }
