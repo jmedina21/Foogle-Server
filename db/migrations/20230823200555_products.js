7d69c3814a9931aa3cff1e62b763cf8e45987abf
Pablo Medina <jmedinamulet@gmail.com>
2023-09-03 20:15:15 -0400

    increase size of image column

--- db/migrations/20230823200555_products.js
+++ db/migrations/20230823200555_products.js
@@ -14,7 +14,7 @@ exports.up = function (knex) {
       table.string('title').notNullable();
       table.string('link').notNullable();
       table.string('price');
-      table.string('image');
+      table.string('image', 1000);
       table.string('location');
       table.timestamp('created_at').defaultTo(knex.fn.now());
       table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
