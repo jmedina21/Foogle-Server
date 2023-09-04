d327e6b9ce96a9a4830bffbd60f992d39b75ec7b
Pablo Medina <jmedinamulet@gmail.com>
2023-09-04 16:40:51 -0400

    update seed to real items

--- db/seeds/02_products.js
+++ db/seeds/02_products.js
@@ -5,60 +5,51 @@
 exports.seed = async function (knex) {
   await knex('products').del();
   await knex('products').insert([
-    {
+    { 
       id: 1,
       user_id: 1,
-      title: "Samsung Galaxy S21 Ultra",
-      image: "123.jpg",
-      link: "https://google.com",
-      price: "$999",
-      location: "Los Angeles",
-    },
-    {
+      title: "The Legend of Zelda Tears of the Kingdom Collector's Edition and CRIS",
+      price: "$120",
+      image: "https://images.craigslist.org/00R0R_10l8wEIjJey_0aT0ew_300x300.jpg",
+      link: "https://newyork.craigslist.org/mnh/vgm/d/new-york-the-legend-of-zelda-tears-of/7662231096.html",
+      location: "Midtown"
+  },
+  { 
       id: 2,
       user_id: 2,
-      title: "Sony WH-1000XM4 Wireless Headphones",
-      image: "123.jpg",
-      link: "https://google.com",
-      price: "$349",
-      location: "Miami",
-    },
-    {
+      title: "Nintendo Switch Console - Animal Crossing: New Horizons Edition",
+      price: "$225",
+      image: null,
+      link: "https://newyork.craigslist.org/mnh/vgm/d/new-york-nintendo-switch-console-animal/7656207423.html",
+      location: "Nolita / Bowery"
+  },
+  { 
       id: 3,
       user_id: 3,
-      title: "Nintendo Switch OLED Model",
-      image: "123.jpg",
-      link: "https://google.com",
-      price: "$150",
-      location: "NYC",
-    },
-    {
+      title: "nintendo switch zelda oled totk console 2 games 1 controller for sale",
+      price: "$500",
+      image: "https://images.craigslist.org/00J0J_3T2xpKEPstT_0t20CI_300x300.jpg",
+      link: "https://newyork.craigslist.org/mnh/vgm/d/new-york-nintendo-switch-zelda-oled/7660457904.html",
+      location: "Midtown"
+  },
+  { 
       id: 4,
       user_id: 1,
-      title: "Samsung Galaxy S21 Ultra",
-      image: "123.jpg",
-      link: "https://google.com",
-      price: "$999",
-      location: "Los Angeles",
-    },
-    {
+      title: "nintendo switch bundle #5 2 games and wired controller for sale",
+      price: "$400",
+      image: "https://images.craigslist.org/00L0L_d0RmzUg8uzR_0t20CI_300x300.jpg",
+      link: "https://newyork.craigslist.org/mnh/vgm/d/new-york-nintendo-switch-bundle-2-games/7660457849.html",
+      location: "Midtown"
+  },
+  { 
       id: 5,
       user_id: 2,
-      title: "Dyson V11 Torque Drive Vacuum Cleaner",
-      image: "123.jpg",
-      link: "https://google.com",
-      price: "$1999",
-      location: "Austin",
-    },
-    {
-      id: 6,
-      user_id: 3,
-      title: "Apple Watch Series 6 (GPS, 40mm) - Blue Aluminum Case with Deep Navy Sport Band",
-      image: "123.jpg",
-      link: "https://google.com",
+      title: "nintendo switch (bundle #4) wired mario controller. 1 game for sale",
       price: "$450",
-      location: "Brooklyn",
-    }
+      image: "https://images.craigslist.org/00n0n_8hBl6MSxxEL_0t20CI_300x300.jpg",
+      link: "https://newyork.craigslist.org/mnh/vgm/d/new-york-nintendo-switch-bundle-wired/7660457789.html",
+      location: "Midtown"
+  },
   ]);
 };
       
