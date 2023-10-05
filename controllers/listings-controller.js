c136b32251355c0b9420820fec33a960adeff61b
Pablo Medina <jmedinamulet@gmail.com>
2023-10-05 12:04:14 -0400

    add redirect request logs and abort

--- controllers/listings-controller.js
+++ controllers/listings-controller.js
@@ -159,36 +159,104 @@ const getEbay = (async (req, res) => {
     res.status(200).json(items)
 })
 
-const shfb = (async (req, res) => {
-  const {search} = req.query;
+// const shfb = (async (req, res) => {
+//   const {search} = req.query;
+
+  
+//   const browser = await puppeteer.launch({
+//     executablePath: process.env.NODE_ENV === 'production'
+//     ? process.env.PUPPETEER_EXECUTABLE_PATH
+//     : puppeteer.executablePath(),
+//     headless: 'new', 
+//     args: ['--no-sandbox', '--disable-setuid-sandbox']
+//   });
+  
+//   const page = await browser.newPage();
+//   page.on('response', async (response) => {
+//     const request = response.request();
+//     const requestURL = request.url();
+//     const responseURL = response.url();
+//     const status = response.status();
+    
+//     console.log(`Request URL: ${requestURL}`);
+//     console.log(`Response URL: ${responseURL}`);
+//     console.log(`Status Code: ${status}`);
+
+//     if (requestURL !== responseURL) {
+//         console.warn('Possible redirect detected.');
+//         // Handle the redirect, e.g., abort, retry, log, etc.
+//     }
+// });
+//   await page.setViewport({width: 1280, height: 800});
+//   await page.goto(`https://www.facebook.com/marketplace/nyc/search/?query=${search}`
+//   //  ,{ waitUntil: 'networkidle2' }
+//   );
+//   console.log(page.url());
+//   const screenshot = await page.screenshot();
+//   await browser.close();
+
+//   res.setHeader('Content-Type', 'image/png');
+//     res.send(screenshot);
+// });
+
+const shfb = async (req, res) => {
+  const { search } = req.query;
 
   const browser = await puppeteer.launch({
     executablePath: process.env.NODE_ENV === 'production'
-    ? process.env.PUPPETEER_EXECUTABLE_PATH
-    : puppeteer.executablePath(),
-    headless: 'new', 
+      ? process.env.PUPPETEER_EXECUTABLE_PATH
+      : puppeteer.executablePath(),
+    headless: 'new',  // Use 'true' for headless mode
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
-  
+
   const page = await browser.newPage();
-  // await page.setUserAgent(randomUserAgent.getRandom());
-  await page.setViewport({width: 1280, height: 800});
-  await page.setCookie([
-    {
-        "name": "wd",
-        "value": "401x553"
+  
+  // Set up the response listener after initializing the 'page' but before navigating
+  page.on('response', async (response) => {
+    const request = response.request();
+    const requestURL = request.url();
+    const responseURL = response.url();
+    const status = response.status();
+
+    console.log(`Request URL: ${requestURL}`);
+    console.log(`Response URL: ${responseURL}`);
+    console.log(`Status Code: ${status}`);
+
+    if (requestURL !== responseURL) {
+      console.warn('Possible redirect detected.');
+      // Handle the redirect, e.g., abort, retry, log, etc.
     }
-]);
-  await page.goto(`https://www.facebook.com/marketplace/nyc/search/?query=${search}`
-  //  ,{ waitUntil: 'networkidle2' }
-  );
+  });
+
+  // Set user agent and viewport after initializing the 'page' and before navigating
+  // await page.setUserAgent(randomUserAgent.getRandom());
+  await page.setViewport({ width: 1280, height: 800 });
+  // await page.goto(`https://www.facebook.com/marketplace/nyc/search/?query=${search}`);
+  page.on('request', (request) => {
+    if (request.url().includes('facebook.com/login')) {
+        console.warn('Redirect detected. Aborting!');
+        request.abort();
+    } else {
+        request.continue();
+    }
+});
+
+try {
+    await page.goto(`https://www.facebook.com/marketplace/nyc/search/?query=${search}`);
+} catch (error) {
+    console.error('Navigation error:', error);
+}
+  
   console.log(page.url());
+  
   const screenshot = await page.screenshot();
   await browser.close();
 
   res.setHeader('Content-Type', 'image/png');
-    res.send(screenshot);
-});
+  res.send(screenshot);
+};
+
 
 module.exports = {
     getCraigslist,
