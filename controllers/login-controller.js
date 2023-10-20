eb198e8a9a4c18a9af8ad28189bb880ee8560135
Pablo Medina <jmedinamulet@gmail.com>
2023-10-20 14:06:43 -0400

    update login with mongoDB

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -3,80 +3,71 @@ const knex = require("knex")(require("../knexfile"));
 const bcrypt = require('bcrypt');
 const jwt_decode = require('jwt-decode');
 require("dotenv").config();
+const User = require('../db/models').User;
 
-const login = (req, res) => {
+const login = async (req, res) => {
     const {email, password} = req.body;
     if(!password || !email){
         return res.status(400).send('Please enter email and password')
     }
-    knex('users')
-        .select('*')
-        .where( {email: email.toLowerCase()})
-        .then((users) => {
-            if (!users.length) {
-                return res.status(401).send('User does not exist')
-            }
-            const isValidPassword = bcrypt.compare(password, users[0].password);
-            if (!isValidPassword) {
-                return res.status(401).send('Incorrect password');
-            }
-            if(!users[0].verified){
-                return res.status(401).send('Please verify your email');
-            }
-            const token = jwt.sign(
-                { id: users[0].id, email: users[0].email },
-                process.env.JWT_SECRET,
-                { expiresIn: "24h" }
-                );
-        
-            res.status(200).json({ token })
-        })
-        .catch((err) => {
-            res.status(500).send('Error logging in user');
-        })
+    try{
+        const user = await User.findOne({email: email.toLowerCase()})
+        if(!user){
+            return res.status(401).send('User does not exist')
+        }
+        const isValidPassword = bcrypt.compare(password, user.password);
+        if(!isValidPassword){
+            return res.status(401).send('Incorrect password');
+        }
+        if(!user.verified){
+            return res.status(401).send('Please verify your email');
+        }
+        const token = jwt.sign(
+            {id: user._id, email: user.email},
+            process.env.JWT_SECRET,
+            {expiresIn: '24h'}
+        )
+
+        res.status(200).json({token})
+    }catch(err){
+        res.status(500).send('Error logging in user', err);
+    }
 }
 
-const loginWithToken = (req, res) => {
+const loginWithToken = async (req, res) => {
     const {token} = req.body;
     if(!token){
         return res.status(401).send('User not authorized');
     }
     const decoded = jwt_decode(token);
-    knex('users')
-        .select('id')
-        .where({email: decoded.email})
-        .then((users) => {
-            if (users.length) {
-                const token = jwt.sign(
-                    { id: users[0].id, email: users[0].email },
-                    process.env.JWT_SECRET,
-                    { expiresIn: "24h" }
-                    );
-                res.status(200).json({ token })
-            } else {
-                knex('users')
-                    .insert({email: decoded.email, verified: true, password: 'password'})
-                    .then(() => {
-                        knex('users')
-                            .select('id')
-                            .where({email: decoded.email})
-                            .then((users) => {
-                                const token = jwt.sign(
-                                    { id: users[0].id, email: users[0].email },
-                                    process.env.JWT_SECRET,
-                                    { expiresIn: "24h" }
-                                    );
-                                    return token
-                                })
-                                .then((token) => {
-                                res.status(200).json({ token })
-                            })
-                    })
-            }
-        })
-        .catch((err) => {
-            res.status(500).send('Error logging in user');
-        })
+    
+    try{
+        const user = await User.findOne({email: decoded.email})
+        if(user){
+            const token = jwt.sign(
+                {id: user._id, email: user.email},
+                process.env.JWT_SECRET,
+                {expiresIn: '24h'}
+            )
+            res.status(200).json({token})
+        }else{
+            const hashedPassword = bcrypt.hash('placeholdePassword', 10)
+            user = new User({
+                email: decoded.email,
+                password: hashedPassword,
+                verified: true
+            })
+            const savedUser = await user.save()
+            const token = jwt.sign(
+                {id: savedUser._id, email: savedUser.email},
+                process.env.JWT_SECRET,
+                {expiresIn: '24h'}
+            )
+            res.status(200).json({token})
+        }
+    }catch(err){
+        res.status(500).send('Error logging in user', err);
+    }
 }
 
 
