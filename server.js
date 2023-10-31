a4e296b4f2df504795e68aafbd04db9cd5fbad6c
Pablo Medina <jmedinamulet@gmail.com>
2023-10-31 11:38:13 -0400

    fix typo cors

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const products = require('./routes/products');
 const validate = require('./routes/validate');
 require("./db/database")();
 
-app.use(cors({origin: 'http://foogle.foo'}))
+app.use(cors({origin: 'https://foogle.foo'}))
 app.use(express.json())
 
 app.get('/', (req, res) => {
